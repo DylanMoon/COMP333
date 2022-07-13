21bf045ef934adc2450a514fdd9fcab47fbd31ab
Dylan Moon <dylan@wiredms.com>
2022-07-13 07:36:38 -0700

    added assignment files, 36/36 passing

---
+++ Assignment1/src/Cons.java
@@ -0,0 +1,48 @@
+public class Cons implements ImmutableList {
+    // ---BEGIN INSTANCE VARIABLES---
+    public final int head;
+    public final ImmutableList tail;
+    // ---END INSTANCE VARIABLES---
+
+    public Cons(final int head, final ImmutableList tail) {
+        this.head = head;
+        this.tail = tail;
+    } // Cons
+
+    public boolean equals(final Object other) {
+        if (other instanceof Cons) {
+            final Cons otherCons = (Cons)other;
+            return head == otherCons.head && tail.equals(otherCons.tail);
+        } else {
+            return false;
+        }
+    } // equals
+
+    @Override
+    public int length() {
+        return 1 + tail.length();
+    }
+
+    @Override
+    public int sum() {
+        return head + tail.sum();
+    }
+
+    @Override
+    public ImmutableList append(ImmutableList other) {
+        return new Cons(head, tail.append(other));
+    }
+
+    @Override
+    public boolean contains(int value) {
+        return head == value || tail.contains(value);
+    }
+
+    public String toString() {
+        return "Cons(" + head + ", " + tail.toString() + ")";
+    } // toString
+
+    public int hashCode() {
+        return sum();
+    } // hashCode
+} // Cons
---
+++ Assignment1/src/Nil.java
@@ -0,0 +1,36 @@
+public class Nil implements ImmutableList {
+    public Nil() {}
+
+    public boolean equals(final Object other) {
+        return other instanceof Nil;
+    } // equals
+
+    @Override
+    public int length() {
+        return 0;
+    }
+
+    @Override
+    public int sum() {
+        return 0;
+    }
+
+    @Override
+    public ImmutableList append(ImmutableList other) {
+        return other;
+    }
+
+    @Override
+    public boolean contains(int value) {
+        return false;
+    }
+
+    public String toString() {
+        return "Nil";
+    } // toString
+
+    public int hashCode() {
+        return 0;
+    } // hashCode
+} // Nil
+    
